174e5532a973cca7b81115efb3f8a583a101fbcc
AnmolDhima12 <anmoldhimans525@gmail.com>
2025-04-21 00:15:37 +0530

    Implement Redux persistence; configure store with persisted reducer and update main.jsx to include PersistGate for state management

--- Frontend/redux/store.jsx
+++ Frontend/redux/store.jsx
@@ -1,6 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user.jsx'
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore'
+import persistReducer from 'redux-persist/es/persistReducer'
+
+const rootReduce = combineReducers({user: userReducer})
+
+const persistConfig = {
+    key:"root",
+    storage,
+    version:1,
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReduce)
 export const store = configureStore({
-    reducer:{user: userReducer},
+    reducer:persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 })
+
+export const persister = persistStore(store);
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from '../redux/store.jsx'
+import { persister, store } from '../redux/store.jsx'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-      <App/>
+      <PersistGate loading={null} persistor={persister}>
+          <App/>
+      </PersistGate>
     </Provider>
 )
